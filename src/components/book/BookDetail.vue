c464fb5fd6fdc0944fc19ffd84287be8327d6584
c_c <1483112868@qq.com>
2022-04-05 14:58:19 +0800

    图书查询 图书位置

--- src/components/book/BookDetail.vue
+++ src/components/book/BookDetail.vue
@@ -5,17 +5,17 @@
     <a-card>
       <a-col :span="16">
         <a-row>
-          <a-col :offset="2" :span="8">
+          <a-col :span="8">
             <a-card
-              title="book_name"
-              style="width: 500px; height: 210px"
+              :title="bookInfo.book_name"
+              style="width: 500px; height: 250px"
               :bordered="true"
             >
-              <p>ISBN：</p>
-              <p>作者：</p>
-              <p>类别：</p>
-              <p>出版社：</p>
-              <p>出版时间：</p>
+              <p>ISBN：{{ bookInfo.ISBN }}</p>
+              <p>作者：{{ bookInfo.author }}</p>
+              <p>类别：{{ bookInfo.category }}</p>
+              <p>出版社：{{ bookInfo.publisher }}</p>
+              <p>出版时间：{{ bookInfo.published_time }}</p>
             </a-card>
           </a-col>
           <a-col :offset="16">
@@ -34,11 +34,9 @@
         </a-row>
 
         <a-row>
-          <a-col :offset="2" :span="8">
+          <a-col :span="8">
             <a-card style="width: 750px; height: 200px" :bordered="true">
-              <p>
-                摘要：https://img4.doubanio.com/view/subject/s/public/s34080469.jpghttps://img4.doubanio.com/view/subject/s/public/s34080469.jpghttps://img4.doubanio.com/view/subject/s/public/s34080469.jpghttps://img4.doubanio.com/view/subject/s/public/s34080469.jpghttps://img4.doubanio.com/view/subject/s/public/s34080469.jpg
-              </p>
+              <p>摘要：{{ bookInfo.introduction }}</p>
             </a-card>
           </a-col>
         </a-row>
@@ -54,13 +52,20 @@
     </a-card>
 
     <a-card>
-      <a-table :columns="columns" :data-source="data">
-        
+      <a-table :columns="columns" :data-source="locationData">
         <template slot="status" slot-scope="status">
           <a-tag v-if="status == 1" color="green">可借阅</a-tag>
           <a-tag v-else-if="status == 0" color="red">不可借阅</a-tag>
         </template>
 
+        <template slot="operation" slot-scope="text, record">
+          <a-popconfirm
+            title="Sure to Borrow?"
+            @confirm="() => onBorrow(record.key)"
+          >
+            <a href="javascript:;">Borrow</a>
+          </a-popconfirm>
+        </template>
       </a-table>
     </a-card>
   </div>
@@ -75,8 +80,8 @@ const columns = [
   },
   {
     title: "bookshelf书架",
-    dataIndex: "bookshelf",
-    key: "bookshelf",
+    dataIndex: "book_shelf",
+    key: "book_shelf",
   },
   {
     title: "side边",
@@ -101,51 +106,107 @@ const columns = [
   },
 ];
 
-const data = [
-  {
-    key: "1",
-    room_number: 'room_number1',
-    bookshelf: 'book_shelf1',
-    side: "side1",
-    layer: "layer1",
-    status: "1",
-  },
-  {
-    key: "2",
-    room_number: 'room_number2',
-    bookshelf: 'book_shelf2',
-    side: "side2",
-    layer: "layer2",
-    status: "1",
-  },
-  {
-    key: "3",
-    room_number: 'room_number3',
-    bookshelf: 'book_shelf3',
-    side: "side3",
-    layer: "layer3",
-    status: "0",
-  },
-];
 
 export default {
   name: "Detail",
   data() {
     return {
-      data,
+      locationData: [],
       columns,
-      id: "",
-      bookInfo: {},
+
+      bookInfo: {
+        ISBN: "",
+        cover: "",
+        book_name: "",
+        introduction: "",
+        publisher: "",
+        published_time: "",
+        author: "",
+        category: "",
+      },
     };
   },
   methods: {
-    getBookId() {
+    getBookISBN() {
       return this.$route.params.id;
+    },
+
+    onBorrow(key) {},
+
+    getBookInfo() {
+      let base_url =
+        "https://www.fastmock.site/mock/891abb0e6bd9ffe5f4e30bde7ed7516e/spm";
+      this.loading = true;
+      let myHeaders = new Headers();
+      let requestoptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: this.getBookISBN(),
+      };
+
+      let that = this;
+      fetch(`${base_url}/api/book/detail`, requestoptions)
+        .then((response) => response.json())
+        .then((result) => {
+          console.log(result);
+          if (result.error_code == 0) {
+            this.bookInfo.ISBN = result.book_info.ISBN;
+            this.bookInfo.cover = result.book_info.cover;
+            this.bookInfo.book_name = result.book_info.book_name;
+            this.bookInfo.introduction = result.book_info.introduction;
+            this.bookInfo.publisher = result.book_info.publisher;
+            this.bookInfo.published_time = result.book_info.published_time;
+            this.bookInfo.author = result.book_info.author;
+            this.bookInfo.category = result.book_info.category;
+          }
+        })
+        .catch((error) => console.log("error", error));
+    },
+
+    getBookLocations() {
+      let base_url =
+        "https://www.fastmock.site/mock/891abb0e6bd9ffe5f4e30bde7ed7516e/spm";
+      this.loading = true;
+      let myHeaders = new Headers();
+      let requestoptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: this.getBookISBN(),
+      };
+
+      let that = this;
+      fetch(`${base_url}/api/book/locations`, requestoptions)
+        .then((response) => response.json())
+        .then((result) => {
+          console.log(result);
+          if (result.error_code == 0) {
+            setTimeout(() => {
+              var key = 0;
+              for (let i of result.locations) {
+                that.locationData.push({
+                  key : key,
+                  status: i.status,
+                  room_number: i.room_number,
+                  book_shelf: i.book_shelf,
+                  side: i.side,
+                  layer: i.layer,
+                });
+                key++;
+              }
+              that.loading = false;
+            }, 200);
+          }
+        })
+        .catch((error) => console.log("error", error));
     },
   },
   created() {
-    this.id = this.getBookId();
-    console.log(this.id);
+    this.id = this.getBookISBN();
+    // console.log(this.id);
+
+    this.getBookInfo();
+    this.getBookLocations();
+    console.log(this.bookInfo.cover);
   },
 };
 </script>
--- src/components/book/BookSearch.vue
+++ src/components/book/BookSearch.vue
@@ -95,27 +95,37 @@
         </a-col>
         <a-col :md="8" :sm="24">
           <a-form-item
-            label="类别"
+            label="图书分类"
             :labelCol="{ span: 5 }"
             :wrapperCol="{ span: 18, offset: 1 }"
           >
-            <a-select
-              v-decorator="[
-                'category',
-                { rules: [{ required: false, message: '' }] },
-              ]"
-              placeholder="请选择"
-            >
-              <!-- 为每个种类设置检索 -->
-              <a-select-option value="1">文学</a-select-option>
-              <a-select-option value="2">科技</a-select-option>
-            </a-select>
+            <a-cascader
+              v-model="cascaderData"
+              ref="text"
+              :options="options"
+              placeholder="请输入或选择分类"
+              :show-search="{ filter }"
+            />
           </a-form-item>
         </a-col>
       </a-row>
 
       <a-row>
-        <a-col :span="24" :style="{ textAlign: 'right' }">
+        <a-col :md="8" :sm="24">
+          <a-form-item
+            label="出版日期"
+            :labelCol="{ span: 5 }"
+            :wrapperCol="{ span: 18, offset: 1 }"
+          >
+            <a-month-picker
+              placeholder="请输入出版日期"
+              style="width: 100%"
+              @change="onChange"
+            />
+          </a-form-item>
+        </a-col>
+
+        <a-col :span="16" :style="{ textAlign: 'right' }">
           <a-button type="primary" html-type="submit"> 查询 </a-button>
           <a-button :style="{ marginLeft: '8px' }" @click="handleReset"
             >重置</a-button
@@ -135,7 +145,6 @@
       >
         <a-list-item slot="renderItem" key="item.ISBN" slot-scope="item">
           <a-list-item-meta>
-            <a-icon type="book" />
             <a slot="title" :href="item.ISBN">{{ item.book_name }}</a>
           </a-list-item-meta>
 
@@ -152,7 +161,7 @@
               评分：<a-rate :default-value="5" disabled />
             </a-col>
             <a-col :span="10" type="flex">
-              <p id="intro">简介：{{ item.intorduction }}</p>
+              <p id="intro">简介：{{ item.introduction }}</p>
             </a-col>
           </a-row>
         </a-list-item>
@@ -162,74 +171,455 @@
 </template>
 
 <script>
-import axios from "axios";
-
-const queryData = async (params) => {
-  axios.get("https://randomuser.me/api", { params: params }).then((res) => {
-    console.log(res.data);
-  });
-  return axios.get("https://randomuser.me/api", { params: params });
-};
-
 export default {
   name: "Search",
 
   data() {
     return {
-      data: [
-        {
-          ISBN: "ISBN1",
-          book_name: "book1",
-          publisher: "publisher1",
-          published_time: "published_time1",
-          author: "author1",
-          cover:
-            "https://img1.doubanio.com/view/subject/s/public/s34080469.jpg",
-          category: "category1",
-          intorduction:
-            "intorduction1https://img1.doubanio.com/view/subject/s/public/s34080469.jpghttps://img1.doubanio.com/view/subject/s/public/s34080469.jpghttps://img1.doubanio.com/view/subject/s/public/s34080469.jpg",
-        },
-        {
-          ISBN: "ISBN2",
-          book_name: "book2",
-          publisher: "publisher2",
-          published_time: "published_time2",
-          author: "author2",
-          cover:
-            "https://img1.doubanio.com/view/subject/s/public/s34080469.jpg",
-          category: "category2",
-          intorduction: "intorduction2",
-        },
-        {
-          ISBN: "ISBN1",
-          book_name: "book3",
-          publisher: "publisher3",
-          published_time: "published_time3",
-          author: "author3",
-          cover:
-            "https://img1.doubanio.com/view/subject/s/public/s34080469.jpg",
-          category: "category3",
-          intorduction: "intorduction3",
-        },
-        {
-          ISBN: "ISBN4",
-          book_name: "book4",
-          publisher: "publisher4",
-          published_time: "published_time4",
-          author: "author4",
-          cover:
-            "https://img1.doubanio.com/view/subject/s/public/s34080469.jpg",
-          category: "category4",
-          intorduction: "intorduction4",
-        },
-      ],
+      cascaderData: [],
+
+      data: [],
       pagination: {
         onChange: (page) => {
           console.log(page);
         },
         pageSize: 10,
       },
-
+      options: [
+        {
+          value: "A",
+          label: "A马克思主义、列宁主义、毛泽东思想、邓小平理论",
+          children: [
+            {
+              value: "A",
+              label: "全部",
+            },
+            {
+              value: "A1",
+              label: "A1马克思、恩格斯著作",
+            },
+            {
+              value: "A2",
+              label: "A2毛泽东著作",
+            },
+            {
+              value: "A3",
+              label:
+                "A3马克思主义、列宁主义、毛泽东思想、邓小平理论的学习和研究",
+            },
+          ],
+        },
+        {
+          value: "B",
+          label: "B哲学、宗教",
+          children: [
+            {
+              value: "B",
+              label: "全部",
+            },
+            {
+              value: "B0",
+              label: "B0哲学理论",
+            },
+            {
+              value: "B81",
+              label: "B81逻辑学",
+            },
+            {
+              value: "B84",
+              label: "B84心理学",
+            },
+          ],
+        },
+        {
+          value: "C",
+          label: "C社会科学总论",
+          children: [
+            {
+              value: "C",
+              label: "全部",
+            },
+            {
+              value: "C0",
+              label: "C0社会科学理论与方法论",
+            },
+            {
+              value: "C91",
+              label: "C91社会学",
+            },
+            {
+              value: "C8",
+              label: "C8统计学",
+            },
+            {
+              value: "C93",
+              label: "C93管理学",
+            },
+          ],
+        },
+        {
+          value: "D",
+          label: "D政治、法律",
+          children: [
+            {
+              value: "D",
+              label: "全部",
+            },
+            {
+              value: "D2",
+              label: "D2中国共产党",
+            },
+            {
+              value: "D8",
+              label: "D8外交、国际关系",
+            },
+            {
+              value: "D9",
+              label: "D9法律",
+            },
+          ],
+        },
+        {
+          value: "E",
+          label: "E军事",
+          children: [
+            {
+              value: "E",
+              label: "全部",
+            },
+            {
+              value: "E0",
+              label: "E0军事理论",
+            },
+            {
+              value: "E2",
+              label: "E2中国军事",
+            },
+          ],
+        },
+        {
+          value: "F",
+          label: "F经济",
+          children: [
+            {
+              value: "F",
+              label: "全部",
+            },
+            {
+              value: "F0",
+              label: "F0经济学",
+            },
+            {
+              value: "F8",
+              label: "F8财政、金融",
+            },
+          ],
+        },
+        {
+          value: "G",
+          label: "G文化、科学、教育、体育",
+          children: [
+            {
+              value: "G",
+              label: "全部",
+            },
+            {
+              value: "G4",
+              label: "G4教育",
+            },
+            {
+              value: "",
+              label: "G8体育",
+            },
+          ],
+        },
+        {
+          value: "H",
+          label: "H语言、文字",
+          children: [
+            {
+              value: "H",
+              label: "全部",
+            },
+            {
+              value: "H0",
+              label: "H0语言学",
+            },
+            {
+              value: "H1",
+              label: "H1汉语",
+            },
+            {
+              value: "H3",
+              label: "H3常用外国语",
+            },
+          ],
+        },
+        {
+          value: "I",
+          label: "I文学",
+          children: [
+            {
+              value: "I",
+              label: "全部",
+            },
+            {
+              value: "I1",
+              label: "I1世界文学",
+            },
+            {
+              value: "I2",
+              label: "I2中国文学",
+            },
+          ],
+        },
+        {
+          value: "J",
+          label: "J艺术",
+          children: [
+            {
+              value: "J",
+              label: "全部",
+            },
+            {
+              value: "J2",
+              label: "J2绘画",
+            },
+            {
+              value: "J9",
+              label: "J9电影、电视艺术",
+            },
+          ],
+        },
+        {
+          value: "K",
+          label: "K历史、地理",
+          children: [
+            {
+              value: "K",
+              label: "全部",
+            },
+            {
+              value: "K1",
+              label: "K1世界史",
+            },
+            {
+              value: "K2",
+              label: "K2中国史",
+            },
+            {
+              value: "K9",
+              label: "K9地理",
+            },
+          ],
+        },
+        {
+          value: "N",
+          label: "N自然科学总论",
+          children: [
+            {
+              value: "N",
+              label: "全部",
+            },
+            {
+              value: "N0",
+              label: "N0自然科学理论与方法论",
+            },
+          ],
+        },
+        {
+          value: "O",
+          label: "O数理科学和化学",
+          children: [
+            {
+              value: "O",
+              label: "全部",
+            },
+            {
+              value: "O1",
+              label: "O1数学",
+            },
+            {
+              value: "O4",
+              label: "O4物理学",
+            },
+            {
+              value: "O6",
+              label: "O6化学",
+            },
+          ],
+        },
+        {
+          value: "P",
+          label: "P天文学、地球科学",
+          children: [
+            {
+              value: "P",
+              label: "全部",
+            },
+            {
+              value: "P1",
+              label: "P1天文学",
+            },
+            {
+              value: "P9",
+              label: "P9自然地理学",
+            },
+          ],
+        },
+        {
+          value: "Q",
+          label: "Q生物科学",
+          children: [
+            {
+              value: "Q",
+              label: "全部",
+            },
+            {
+              value: "Q2",
+              label: "Q2细胞生物学",
+            },
+            {
+              value: "Q4",
+              label: "Q4遗传学",
+            },
+            {
+              value: "Q94",
+              label: "Q94植物学",
+            },
+          ],
+        },
+        {
+          value: "R",
+          label: "R医药、卫生",
+          children: [
+            {
+              value: "R",
+              label: "全部",
+            },
+            {
+              value: "R3",
+              label: "R3基础医学",
+            },
+            {
+              value: "R4",
+              label: "R4临床医学",
+            },
+            {
+              value: "R9",
+              label: "R9药学",
+            },
+          ],
+        },
+        {
+          value: "S",
+          label: "S农业科学",
+          children: [
+            {
+              value: "S",
+              label: "全部",
+            },
+            {
+              value: "S1",
+              label: "S1农业基础科学",
+            },
+            {
+              value: "S6",
+              label: "S6园艺",
+            },
+          ],
+        },
+        {
+          value: "T",
+          label: "T工业技术",
+          children: [
+            {
+              value: "T",
+              label: "全部",
+            },
+            {
+              value: "TB",
+              label: "TB一般工业技术",
+            },
+            {
+              value: "TN",
+              label: "TN无线电电子学、电信技术",
+            },
+            {
+              value: "TP",
+              label: "TP自动化技术、计算机技术",
+            },
+          ],
+        },
+        {
+          value: "U",
+          label: "U交通运输",
+          children: [
+            {
+              value: "U",
+              label: "全部",
+            },
+            {
+              value: "U2",
+              label: "U2铁路运输",
+            },
+            {
+              value: "U4",
+              label: "U4公路运输",
+            },
+            {
+              value: "U6",
+              label: "U6水路运输",
+            },
+          ],
+        },
+        {
+          value: "V",
+          label: "V航空、航天",
+          children: [
+            {
+              value: "V",
+              label: "全部",
+            },
+            {
+              value: "V2",
+              label: "V2航空",
+            },
+            {
+              value: "V4",
+              label: "V4航天",
+            },
+          ],
+        },
+        {
+          value: "X",
+          label: "X环境科学、安全科学",
+          children: [
+            {
+              value: "X",
+              label: "全部",
+            },
+            {
+              value: "X1",
+              label: "X1环境科学基础理论",
+            },
+            {
+              value: "X3",
+              label: "X3环境保护管理",
+            },
+          ],
+        },
+        {
+          value: "Z",
+          label: "Z综合类图书",
+          children: [
+            {
+              value: "Z",
+              label: "全部",
+            },
+          ],
+        },
+      ],
       //查询表单
       form: this.$form.createForm(this, { name: "advanced_search" }),
     };
@@ -250,59 +640,65 @@ export default {
     handleSearch(e) {
       e.preventDefault();
       this.form.validateFields((error, values) => {
-        console.log("error", error);
+        // console.log("error", error);
         console.log("Received values of form: ", values);
 
-        this.data.push({
-          ISBN: "ISBN1",
-          book_name: "book1",
-          publisher: "publisher1",
-          published_time: "published_time1",
-          author: "author1",
-          cover:
-            "https://img1.doubanio.com/view/subject/s/public/s34080469.jpg",
-          category: "category1",
-          intorduction:
-            "intorduction1https://img1.doubanio.com/view/subject/s/public/s34080469.jpghttps://img1.doubanio.com/view/subject/s/public/s34080469.jpghttps://img1.doubanio.com/view/subject/s/public/s34080469.jpg",
-        });
+        values.category = this.cascaderData;
+
+        let base_url =
+          "https://www.fastmock.site/mock/891abb0e6bd9ffe5f4e30bde7ed7516e/spm";
+        this.loading = true;
+        let myHeaders = new Headers();
+        let requestoptions = {
+          method: "POST",
+          headers: myHeaders,
+          body: values,
+        };
+
+        let that = this;
+        fetch(`${base_url}/api/book/search`, requestoptions)
+          .then((response) => response.json())
+          .then((result) => {
+            console.log(result);
+            if (result.error_code == 0 || result.error_code == "0") {
+              this.data.splice(0, this.data.length);
+              setTimeout(() => {
+                for (let i of result.search_result) {
+                  that.data.push({
+                    ISBN: i.ISBN,
+                    cover: i.cover,
+                    book_name: i.book_name,
+                    introduction: i.introduction,
+                    publisher: i.publisher,
+                    published_time: i.published_time,
+                    author: i.author,
+                    category: i.category,
+                  });
+                }
+                that.loading = false;
+              }, 200);
+            }
+          })
+          .catch((error) => console.log("error", error));
       });
     },
 
     handleReset() {
       console.log("重置属性");
       this.form.resetFields();
+      this.$refs.text.sValue = [];
     },
 
-    // //处理分页
-    // handleTableChange(pagination, filters, sorter) {
-    //   console.log(pagination);
-    //   const pager = { ...this.pagination };
-    //   pager.current = pagination.current;
-    //   this.pagination = pager;
-    //   this.fetch({
-    //     results: pagination.pageSize,
-    //     page: pagination.current,
-    //     sortField: sorter.field,
-    //     sortOrder: sorter.order,
-    //     ...filters,
-    //   });
-    // },
+    filter(inputValue, path) {
+      return path.some(
+        (option) =>
+          option.label.toLowerCase().indexOf(inputValue.toLowerCase()) > -1
+      );
+    },
 
-    // fetch(params = {}) {
-    //   this.loading = true;
-    //   queryData({
-    //     results: 10,
-    //     ...params,
-    //   }).then(({ data }) => {
-    //     const pagination = { ...this.pagination };
-    //     // Read total count from server
-    //     // pagination.total = data.totalCount;
-    //     pagination.total = 200;
-    //     this.loading = false;
-    //     this.data = data.results;
-    //     this.pagination = pagination;
-    //   });
-    // },
+    onChange() {
+      this.$message.info("表格状态改变了");
+    },
   },
 
   created() {
