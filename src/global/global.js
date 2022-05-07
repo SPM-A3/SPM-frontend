988d025e257491c6fab969f8a9513df199981812
王泽睿 <1404894808@qq.com>
2022-05-07 22:41:48 +0800

    fix ui

--- src/global/global.js
+++ src/global/global.js
@@ -1,7 +1,8 @@
 // 接口地址
 // const BASE_URL = "http://175.24.201.104:8085";
 const BASE_URL = "http://101.43.38.236:8085";
-const EXTRA_FUNCTION = "http://localhost:3000"
+const EXTRA_FUNCTION = "https://1893791694056142.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/web-framework/extra-function"
+// const EXTRA_FUNCTION = "http://localhost:3000"
 const IS_LOGIN = false;
 export default {
   BASE_URL,
