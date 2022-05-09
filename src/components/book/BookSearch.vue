22379e54343507fda1734bb9c523362ed3d6e9b0
c_c <1483112868@qq.com>
2022-05-09 15:29:52 +0800

    add locations in search page

--- src/components/book/BookSearch.vue
+++ src/components/book/BookSearch.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="book-search">
-    <!-- from表单搜索部件 -->
     <a-form class="search-form" :form="form" @submit="handleSearch">
       <a-row>
         <a-col :md="8" :sm="24">
@@ -143,9 +142,25 @@
               <p><b>ISBN</b>: {{ item.ISBN }}</p>
               <p><b>Available</b>: <a-tag color="green"> YES </a-tag></p>
             </a-col>
-            <!-- <a-col :span="10" type="flex">
-              <p id="intro"><b>Introduction</b>: {{ item.introduction }}</p>
-            </a-col> -->
+            <a-col :span="8" :offset="2" type="flex">
+              <a-popover title="Book Location" placement="bottomLeft">
+                <template #content>
+                  <a-table
+                    :columns="location_columns"
+                    :data-source="location_data"
+                    rowKey="book_id"
+                  >
+                    <template slot="status" slot-scope="status">
+                      <a-tag v-if="status == 0" color="green">Available</a-tag>
+                      <a-tag v-else-if="status == 1" color="red">Lent</a-tag>
+                    </template>
+                  </a-table>
+                </template>
+                <a-button @mouseover="getLocationByISBN(item.ISBN)"
+                  >Locations</a-button
+                >
+              </a-popover>
+            </a-col>
           </a-row>
         </a-list-item>
       </a-list>
@@ -155,7 +170,7 @@
 
 <script>
 import { getAccessToken } from "@/services/user";
-import  BASE_URL  from "@/services/api";
+import options from "../admin/category";
 
 export default {
   name: "Search",
@@ -166,462 +181,50 @@ export default {
       DateValue: "",
 
       data: [],
+
       pagination: {
         onChange: (page) => {
           console.log(page);
         },
         pageSize: 10,
       },
-      options: [
-        {
-          value: "A",
-          label: "A马克思主义、列宁主义、毛泽东思想、邓小平理论",
-          children: [
-            {
-              value: "A",
-              label: "全部",
-            },
-            {
-              value: "A1",
-              label: "A1马克思、恩格斯著作",
-            },
-            {
-              value: "A2",
-              label: "A2毛泽东著作",
-            },
-            {
-              value: "A3",
-              label:
-                "A3马克思主义、列宁主义、毛泽东思想、邓小平理论的学习和研究",
-            },
-          ],
-        },
-        {
-          value: "B",
-          label: "B哲学、宗教",
-          children: [
-            {
-              value: "B",
-              label: "全部",
-            },
-            {
-              value: "B0",
-              label: "B0哲学理论",
-            },
-            {
-              value: "B81",
-              label: "B81逻辑学",
-            },
-            {
-              value: "B84",
-              label: "B84心理学",
-            },
-          ],
-        },
-        {
-          value: "C",
-          label: "C社会科学总论",
-          children: [
-            {
-              value: "C",
-              label: "全部",
-            },
-            {
-              value: "C0",
-              label: "C0社会科学理论与方法论",
-            },
-            {
-              value: "C91",
-              label: "C91社会学",
-            },
-            {
-              value: "C8",
-              label: "C8统计学",
-            },
-            {
-              value: "C93",
-              label: "C93管理学",
-            },
-          ],
-        },
-        {
-          value: "D",
-          label: "D政治、法律",
-          children: [
-            {
-              value: "D",
-              label: "全部",
-            },
-            {
-              value: "D2",
-              label: "D2中国共产党",
-            },
-            {
-              value: "D8",
-              label: "D8外交、国际关系",
-            },
-            {
-              value: "D9",
-              label: "D9法律",
-            },
-          ],
-        },
-        {
-          value: "E",
-          label: "E军事",
-          children: [
-            {
-              value: "E",
-              label: "全部",
-            },
-            {
-              value: "E0",
-              label: "E0军事理论",
-            },
-            {
-              value: "E2",
-              label: "E2中国军事",
-            },
-          ],
-        },
-        {
-          value: "F",
-          label: "F经济",
-          children: [
-            {
-              value: "F",
-              label: "全部",
-            },
-            {
-              value: "F0",
-              label: "F0经济学",
-            },
-            {
-              value: "F8",
-              label: "F8财政、金融",
-            },
-          ],
-        },
-        {
-          value: "G",
-          label: "G文化、科学、教育、体育",
-          children: [
-            {
-              value: "G",
-              label: "全部",
-            },
-            {
-              value: "G4",
-              label: "G4教育",
-            },
-            {
-              value: "",
-              label: "G8体育",
-            },
-          ],
-        },
-        {
-          value: "H",
-          label: "H语言、文字",
-          children: [
-            {
-              value: "H",
-              label: "全部",
-            },
-            {
-              value: "H0",
-              label: "H0语言学",
-            },
-            {
-              value: "H1",
-              label: "H1汉语",
-            },
-            {
-              value: "H3",
-              label: "H3常用外国语",
-            },
-          ],
-        },
-        {
-          value: "I",
-          label: "I文学",
-          children: [
-            {
-              value: "I",
-              label: "全部",
-            },
-            {
-              value: "I1",
-              label: "I1世界文学",
-            },
-            {
-              value: "I2",
-              label: "I2中国文学",
-            },
-          ],
-        },
-        {
-          value: "J",
-          label: "J艺术",
-          children: [
-            {
-              value: "J",
-              label: "全部",
-            },
-            {
-              value: "J2",
-              label: "J2绘画",
-            },
-            {
-              value: "J9",
-              label: "J9电影、电视艺术",
-            },
-          ],
-        },
-        {
-          value: "K",
-          label: "K历史、地理",
-          children: [
-            {
-              value: "K",
-              label: "全部",
-            },
-            {
-              value: "K1",
-              label: "K1世界史",
-            },
-            {
-              value: "K2",
-              label: "K2中国史",
-            },
-            {
-              value: "K9",
-              label: "K9地理",
-            },
-          ],
-        },
-        {
-          value: "N",
-          label: "N自然科学总论",
-          children: [
-            {
-              value: "N",
-              label: "全部",
-            },
-            {
-              value: "N0",
-              label: "N0自然科学理论与方法论",
-            },
-          ],
-        },
-        {
-          value: "O",
-          label: "O数理科学和化学",
-          children: [
-            {
-              value: "O",
-              label: "全部",
-            },
-            {
-              value: "O1",
-              label: "O1数学",
-            },
-            {
-              value: "O4",
-              label: "O4物理学",
-            },
-            {
-              value: "O6",
-              label: "O6化学",
-            },
-          ],
-        },
-        {
-          value: "P",
-          label: "P天文学、地球科学",
-          children: [
-            {
-              value: "P",
-              label: "全部",
-            },
-            {
-              value: "P1",
-              label: "P1天文学",
-            },
-            {
-              value: "P9",
-              label: "P9自然地理学",
-            },
-          ],
-        },
-        {
-          value: "Q",
-          label: "Q生物科学",
-          children: [
-            {
-              value: "Q",
-              label: "全部",
-            },
-            {
-              value: "Q2",
-              label: "Q2细胞生物学",
-            },
-            {
-              value: "Q4",
-              label: "Q4遗传学",
-            },
-            {
-              value: "Q94",
-              label: "Q94植物学",
-            },
-          ],
-        },
-        {
-          value: "R",
-          label: "R医药、卫生",
-          children: [
-            {
-              value: "R",
-              label: "全部",
-            },
-            {
-              value: "R3",
-              label: "R3基础医学",
-            },
-            {
-              value: "R4",
-              label: "R4临床医学",
-            },
-            {
-              value: "R9",
-              label: "R9药学",
-            },
-          ],
-        },
-        {
-          value: "S",
-          label: "S农业科学",
-          children: [
-            {
-              value: "S",
-              label: "全部",
-            },
-            {
-              value: "S1",
-              label: "S1农业基础科学",
-            },
-            {
-              value: "S6",
-              label: "S6园艺",
-            },
-          ],
-        },
-        {
-          value: "T",
-          label: "T工业技术",
-          children: [
-            {
-              value: "T",
-              label: "全部",
-            },
-            {
-              value: "TB",
-              label: "TB一般工业技术",
-            },
-            {
-              value: "TN",
-              label: "TN无线电电子学、电信技术",
-            },
-            {
-              value: "TP",
-              label: "TP自动化技术、计算机技术",
-            },
-          ],
-        },
-        {
-          value: "U",
-          label: "U交通运输",
-          children: [
-            {
-              value: "U",
-              label: "全部",
-            },
-            {
-              value: "U2",
-              label: "U2铁路运输",
-            },
-            {
-              value: "U4",
-              label: "U4公路运输",
-            },
-            {
-              value: "U6",
-              label: "U6水路运输",
-            },
-          ],
-        },
-        {
-          value: "V",
-          label: "V航空、航天",
-          children: [
-            {
-              value: "V",
-              label: "全部",
-            },
-            {
-              value: "V2",
-              label: "V2航空",
-            },
-            {
-              value: "V4",
-              label: "V4航天",
-            },
-          ],
-        },
-        {
-          value: "X",
-          label: "X环境科学、安全科学",
-          children: [
-            {
-              value: "X",
-              label: "全部",
-            },
-            {
-              value: "X1",
-              label: "X1环境科学基础理论",
-            },
-            {
-              value: "X3",
-              label: "X3环境保护管理",
-            },
-          ],
-        },
-        {
-          value: "Z",
-          label: "Z综合类图书",
-          children: [
-            {
-              value: "Z",
-              label: "全部",
-            },
-          ],
-        },
-      ],
+      options: options.options,
       //查询表单
       form: this.$form.createForm(this, { name: "advanced_search" }),
+
+      location_columns: [
+        {
+          title: "Room Number",
+          dataIndex: "room_number",
+          key: "room_number",
+        },
+        {
+          title: "Bookshelf",
+          dataIndex: "book_shelf",
+          key: "book_shelf",
+        },
+        {
+          title: "Side",
+          dataIndex: "side",
+          key: "side",
+        },
+        {
+          title: "Layer",
+          dataIndex: "layer",
+          key: "layer",
+        },
+        {
+          title: "Status",
+          dataIndex: "status",
+          key: "status",
+          scopedSlots: { customRender: "status" },
+        },
+      ],
+      location_data: [],
     };
   },
   computed: {},
 
-  filters: {
-    //解决豆瓣图书api图片无法显示的问题
-    imgChange(str) {
-      return str.replace(/img./, "img4");
-    },
-  },
-  props: {
-    msg: String,
-  },
   methods: {
     //查询事件的处理函数
     handleSearch(e) {
@@ -645,9 +248,6 @@ export default {
         if (this.DateValue) values.pub_month = this.DateValue.month() + 1;
         else values.pub_month = "";
 
-        // let BASE_URL ="https://www.fastmock.site/mock/891abb0e6bd9ffe5f4e30bde7ed7516e/spm";
-        let BASE_URL = "http://175.24.201.104:8085"
-        
         this.loading = true;
         let myHeaders = new Headers();
         myHeaders.append("token", getAccessToken());
@@ -660,13 +260,10 @@ export default {
 
         let that = this;
         fetch(
-          `${BASE_URL}/api/book/search?book_name=${values.book_name}&author=${values.author}&publisher=${values.publisher}&ISBN=${values.ISBN}&category=${values.category}&pub_year=${values.pub_year}&pub_month=${values.pub_month}`,
+          `${this.$global.BASE_URL}/api/book/search?book_name=${values.book_name}&author=${values.author}&publisher=${values.publisher}&ISBN=${values.ISBN}&category=${values.category}&pub_year=${values.pub_year}&pub_month=${values.pub_month}`,
           requestoptions
         )
           .then((response) => response.json())
-          //  .then(res => res.text())        
-          // .then(text => console.log(text)) 
-          
           .then((result) => {
             console.log(result);
             if (result.code == 0) {
@@ -697,6 +294,39 @@ export default {
       console.log("重置属性");
       this.form.resetFields();
       this.$refs.text.sValue = [];
+    },
+
+    getLocationByISBN(ISBN) {
+      this.location_data = [];
+      let myHeaders = new Headers();
+      myHeaders.append("token", getAccessToken());
+      let requestoptions = {
+        method: "GET",
+        headers: myHeaders,
+      };
+
+      let that = this;
+      fetch(
+        `${this.$global.BASE_URL}/api/book/locations?ISBN=${ISBN}`,
+        requestoptions
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          console.log(result);
+          if (result.code == 0) {
+            for (let i of result.data) {
+              that.location_data.push({
+                book_id: i.book_id,
+                status: i.status,
+                room_number: i.room_number,
+                book_shelf: i.book_shelf,
+                side: i.side,
+                layer: i.layer,
+              });
+            }
+          }
+        })
+        .catch((error) => console.log("error", error));
     },
 
     filter(inputValue, path) {
